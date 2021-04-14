469104be22dffdefdb8b635a0aba8d0f98b445c4
Fritz Louis Wilke <fritz.wilke@tu-dresden.de>
2021-04-14 18:40:45 +0200

    added calculations for amdahls law

--- README.md
+++ README.md
@@ -23,9 +23,15 @@ mopp-2018-t0-harmonic-progression-sum; 1; 48; 6561099
 mopp-2018-t0-harmonic-progression-sum; 1; 56; 5672094
 ```
 
-**The graph:**
+**Speedup Graph:**
 
-![Harmonic Progression Sum Performance](plotting/plots/harmonic-progression.png)
+![Harmonic Progression Sum Performance](plotting/plots/harmonic-progression-scaling.png)
+
+**Amdahl's law with measurements:**
+
+![Amdahl's Law](plotting/plots/harmonic-progression-amdahls-law.png)
+
+The graph contains the measurements from above as well as two flows of **Amdahl's Law**. The first one is calculated from the speedup **with 28 cores**, the second form the speedup **with 56 cores**.
 
 ## Task 2:
 
--- plotting/index.js
+++ plotting/index.js
@@ -15,23 +15,79 @@ if (!fs.existsSync(LOG_DIR)) fs.mkdirSync(LOG_DIR);
 // array of log filenames without the ending ".csv"
 const LOG_FILENAMES = fs.readdirSync(LOG_DIR, { withFileTypes: true }).filter( f => f.isFile() && f.name.endsWith(".csv") ).map( f => f.name.slice(0, -4) );
 
+/** @type {number[]} Contains the numbers 2^x for x = 0..16 */
+const AMDAHLS_LAW_X = [];
+for (let i=0;i<=16;i++) AMDAHLS_LAW_X.push(Math.pow(2, i));
+
+/**
+ * Calculates the parallelized code part of the measured program
+ * after Amdahl's Law
+ * @param {number} speedup The speedup in comparison to using
+ * only one CPU core
+ * @param {number} numCores The number of cores the was used
+ * when the given speedup was achieved
+ */
+function getParallelCodeBySpeedup( speedup, numCores ) {
+    const s = speedup, n = numCores;
+    return (n/s) * (1/(1-n)) - (n/(1-n));
+}
+
+/**
+ * Calculates the speedup with the given parallel code base
+ * portion and the number of cores
+ * @param {number} parallelCodePart 
+ * @param {number} numCores The number of cores the was used
+ * when the given speedup was achieved
+ */
+function amdahlsLaw( parallelCodePart, numCores ) {
+    const p = parallelCodePart, n = numCores;
+    return 1 / ( (1-p) + (p/n) );
+}
+
+/**
+ * @param {{ data: import("plotly.js").Data[], layout: import("plotly.js").Layout }} figure 
+ * @param {import("fs").PathLike} filepath 
+ */
+async function createPlot( figure, filepath ) {
+
+    /** @type {import("http").IncomingMessage} */
+    const imgStream = await new Promise( (resolve, reject) => 
+        plotly.getImage(
+            figure,
+            {
+                format: "png",
+                width: 1000,
+                height: 500
+            },
+            (err, msg) => err ? reject([err, msg]) : resolve(msg)
+        )
+    );
+
+    // write to fs
+    const fileStream = fs.createWriteStream(filepath);
+    await new Promise( (resolve, reject) => pipeline(imgStream, fileStream, err => err ? reject(err) : resolve()) );
+    console.log(`Created plot ${path.basename(filepath)}`);
+
+}
+
 async function main() {
 
     // plot one image per log
     for (const filename of LOG_FILENAMES) {
 
-        console.log(`Processing logs in ${filename}.csv ...`);
+        console.log(`Processing logs for ${filename} ...`);
         const logFilepath = path.join(LOG_DIR, filename+".csv");
-        const plotFilepath = path.join(PLOT_DIR, filename+".png");
+        const scalingPlotFilepath = path.join(PLOT_DIR, filename+"-scaling.png");
+        const parallelCodePlotFilepath = path.join(PLOT_DIR, filename+"-amdahls-law.png");
         const log = fs.readFileSync(logFilepath, "utf8");
-        let plotname = filename;
+        let scalingPlotname = filename;
 
         // extract data
         /** @type {Map<number, number[]>} Cores => Times */
         const data = new Map();
         log.split("\n").map( l => l.split(";").map( w => w.trim() ) ).forEach( l => {
-            if (l.length !== 4) return;
-            if (data.size === 0) plotname = l[0];
+            if (l.length !== 4 || isNaN(Number.parseInt(l[3]))) return;
+            if (data.size === 0) scalingPlotname = l[0];
             const cores = Number.parseInt(l[2]);
             if (!data.has(cores)) data.set(cores, []);
             data.get(cores).push(Number.parseInt(l[3]));
@@ -45,9 +101,14 @@ async function main() {
 
         // calculate mean time over all runs
         const meanData = new Map( Array.from(data).map( ([cores, times]) => [cores, times.reduce( (sum, t) => sum+t, 0 )/times.length] ) );
+        const basetime = meanData.get(1); // time im microsecs for 1 core
+
+        // calculate amdahls law related data
+        const amdahlsLawData = new Map( Array.from(meanData).map( ([cores, time]) => [cores, getParallelCodeBySpeedup(basetime/time, cores)] ) );
+        const p28 = amdahlsLawData.get(28); const p56 = amdahlsLawData.get(56);
+        console.log(`Parellized code is ${(p28*100).toFixed(2)}% @28 cores and ${(p56*100).toFixed(2)}% @56 cores`);
 
         // create data from log lines
-        const basetime = meanData.get(1); // time im microsecs for 1 core
         /** @type {number[]} */ const x = [];
         /** @type {number[]} */ const y = [];
         for (const [cores, meanTime] of meanData) {
@@ -55,49 +116,95 @@ async function main() {
             y.push(basetime/meanTime);  // speedup
         }
 
-        // create plot
-        console.log("Creating plot ...");
-        /** @type {import("http").IncomingMessage} */
-        const imgStream = await new Promise( (resolve, reject) => 
-            plotly.getImage(
-                {
-                    data: [{
+        // create scaling plot
+        await createPlot(
+            {
+                data: [{
+                    x,
+                    y,
+                    type: "scatter",
+                    line: {
+                        width: 5
+                    },
+                    marker: {
+                        size: 10
+                    }
+                }],
+                layout: {
+                    xaxis: {
+                        title: "CPU-Cores",
+                        tickvals: x,
+                        tickmode: "array",
+                        type: "log"
+                    },
+                    yaxis: {
+                        title: "Speedup"
+                    },
+                    title: scalingPlotname
+                }
+            },
+            scalingPlotFilepath
+        );
+
+        // create amdahl's law plot
+        await createPlot(
+            {
+                data: [
+                    {
+                        x: AMDAHLS_LAW_X,
+                        y: AMDAHLS_LAW_X.map( n => amdahlsLaw(p28, n) ),
+                        type: "scatter",
+                        line: {
+                            width: 2
+                        },
+                        marker: {
+                            size: 0
+                        },
+                        name: `Amdahl's Law; p=${(p28*100).toFixed(2)}%`
+                    },
+                    {
+                        x: AMDAHLS_LAW_X,
+                        y: AMDAHLS_LAW_X.map( n => amdahlsLaw(p56, n) ),
+                        type: "scatter",
+                        line: {
+                            width: 2,
+                            dash: "dash"
+                        },
+                        marker: {
+                            size: 0
+                        },
+                        name: `Amdahl's Law; p=${(p56*100).toFixed(2)}%`
+                    },
+                    {
                         x,
                         y,
                         type: "scatter",
                         line: {
-                            width: 5
+                            width: 2,
+                            dash: "dot"
                         },
                         marker: {
-                            size: 10
-                        }
-                    }],
-                    layout: {
-                        xaxis: {
-                            title: "CPU-Cores",
-                            tickvals: x,
-                            tickmode: "array",
-                            type: "log"
+                            size: 6
                         },
-                        yaxis: {
-                            title: "Speedup"
-                        },
-                        title: plotname
+                        name: "Measurement"
                     }
-                },
-                {
-                    format: "png",
-                    width: 1000,
-                    height: 500
-                },
-                (err, msg) => err ? reject(err) : resolve(msg)
-            )
+                ],
+                layout: {
+                    xaxis: {
+                        title: "CPU-Cores",
+                        tickvals: AMDAHLS_LAW_X,
+                        tickmode: "array",
+                        type: "log"
+                    },
+                    yaxis: {
+                        title: "Speedup"
+                    },
+                    title: "Amdahl's law",
+                    showlegend: true
+                }
+            },
+            parallelCodePlotFilepath
         );
-
-        // write to fs
-        const fileStream = fs.createWriteStream(plotFilepath);
-        await new Promise( (resolve, reject) => pipeline(imgStream, fileStream, err => err ? reject(err) : resolve()) );
-        console.log(`Created plot ${filename}.png`)
 
     }
 
