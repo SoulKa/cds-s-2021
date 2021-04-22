900e3d20e929c9a15fcc6c79ddb49cc1c31b97f0
Fritz Louis Wilke <fritz.wilke@tu-dresden.de>
2021-04-22 11:25:46 +0200

    updated the matrix class

--- tasks/mopp-2018-t3-himeno/matrix.cpp
+++ tasks/mopp-2018-t3-himeno/matrix.cpp
@@ -1,33 +1,16 @@
 #include "matrix.h"
 
+Matrix::Matrix( uint nums, uint rows, uint cols, uint deps )
+    : m_uiNums(nums), m_uiRows(rows), m_uiCols(cols), m_uiDeps(deps), m_pData(new float[nums * rows * cols * deps * sizeof(float)]) {}
+
 Matrix::~Matrix() {
-    clear();
+    if (m_pData != nullptr) delete[] m_pData;
 }
 
-void Matrix::initialize( uint nums, uint rows, uint cols, uint deps ) {
-    m_uiNums = nums;
-    m_uiRows = rows;
-    m_uiCols = cols;
-    m_uiDeps = deps;
-    m_pData = new float[nums * rows * cols * deps * sizeof(float)];
-}
-
-void Matrix::clear() {
-
-    if (m_pData == nullptr) return;
-
-    delete[] m_pData;
-    m_uiNums = 0;
-    m_uiRows = 0;
-    m_uiCols = 0;
-    m_uiDeps = 0;
-
-}
-
-void Matrix::set( int l, float value ) {
+void Matrix::set( int n, float value ) {
     for (uint i=0; i<m_uiRows; i++) {
         for (uint j=0; j<m_uiCols; j++) {
-            for (uint k=0; k<m_uiDeps; k++) at(l,i,j,k) = value;
+            for (uint k=0; k<m_uiDeps; k++) at(n,i,j,k) = value;
         } 
     }
 }
@@ -44,6 +27,14 @@ float & Matrix::at( uint n, uint r, uint c, uint d ) {
     return m_pData[(n) * m_uiRows * m_uiCols * m_uiDeps + (r) * m_uiCols * m_uiDeps + (c) * m_uiDeps + (d)];
 }
 
-void copy( Matrix *src, Matrix *dst, uint n_begin, uint r_begin, uint c_begin, uint d_begin, uint n_end, uint r_end, uint c_end, uint d_end ) {
+void Matrix::copy( Matrix *src, Matrix *dst, uint n_begin, uint r_begin, uint c_begin, uint d_begin, uint n_end, uint r_end, uint c_end, uint d_end ) {
+
+    for (; n_begin < n_end; n_begin++) {
+        for (; r_begin < r_end; r_begin++) {
+            for (; c_begin < c_end; c_begin++) {
+                for (; c_begin < c_end; c_begin++) dst->at(n_begin, r_begin, c_begin, d_begin) = src->at(n_begin, r_begin, c_begin, d_begin);
+            }
+        }
+    }
 
 }
--- tasks/mopp-2018-t3-himeno/matrix.h
+++ tasks/mopp-2018-t3-himeno/matrix.h
@@ -7,27 +7,50 @@ class Matrix {
 
     public:
 
-        float* m_pData = nullptr;
+        const uint m_uiNums = 0;
+        const uint m_uiRows = 0;
+        const uint m_uiCols = 0;
+        const uint m_uiDeps = 0;
+        float* const m_pData = nullptr;
 
-        uint m_uiNums = 0;
-
-        uint m_uiRows = 0;
-
-        uint m_uiCols = 0;
-
-        uint m_uiDeps = 0;
-
+        Matrix() {}
+        Matrix( uint nums, uint rows, uint cols, uint deps );
         ~Matrix();
 
-        void initialize( uint nums, uint rows, uint cols, uint deps );
-
-        void clear();
-
-        void set(int l, float val);
+        /**
+         * @brief Fills the whole matrix at num n with the given
+         * value
+         * @param n The num/level to access
+         * @param val The value to fill the matrix with
+         */
+        void set(int n, float val);
 
         void set_init();
 
+        /**
+         * @brief Access the matrix at the given position
+         * @param num The num to access it at
+         * @param row The row to access it at
+         * @param col The column to access it at
+         * @param depth The depth to access it at
+         * @return A reference to the float at the given position
+         */
         float & at( uint num, uint row, uint col, uint depth );
+
+        /**
+         * @brief Copies the contents of given src matrix into the dst matrix
+         * @param src The source
+         * @param dst The destination
+         * @param n_begin The first num to copy
+         * @param r_begin The first row to copy
+         * @param c_begin The first column to copy
+         * @param d_begin The first depth to copy
+         * @param n_end The num to end copying (excluding)
+         * @param r_end The row to end copying (excluding)
+         * @param c_end The column to end copying (excluding)
+         * @param d_end The depth to end copying (excluding)
+         */
+        static void copy( Matrix *src, Matrix *dst, uint n_begin, uint r_begin, uint c_begin, uint d_begin, uint n_end, uint r_end, uint c_end, uint d_end );
 
 };
 
