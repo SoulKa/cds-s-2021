6543e3a9902cbc2f76175d0047e8a42365623e02
Fritz Louis Wilke <fritz.wilke@tu-dresden.de>
2021-04-23 20:13:06 +0200

    improved mandelbrot by having a bigger stepsize for workingset

--- tasks/mopp-2017-t3-mandelbrot-set/mandelbrot.cpp
+++ tasks/mopp-2017-t3-mandelbrot-set/mandelbrot.cpp
@@ -2,60 +2,84 @@
 #include <iostream>
 #include <thread>
 #include <mutex>
+#include <atomic>
 
 #include <assert.h>
 
 using namespace std;
 
 // TYPEDEFS
+
 typedef struct {
-    const unsigned int MAX_ROW;
-    const unsigned int MAX_COL;
-    const unsigned int MAX_N;
+    const uint MAX_ROW;
+    const uint MAX_COL;
+    const uint MAX_N;
     const float COL_ITER;
     const float ROW_ITER;
     char *IMAGE;
 } s_constants_t;
 
+typedef unsigned int uint;
+
+
+
 // GLOBAL VARS
-mutex pixel_mutex;
-unsigned int current_pixel = 0;
+
+atomic<uint> current_pixel(0);
+uint NUM_CORES;
+
 
 // FUNCTIONS
-
-void calculate( unsigned int r, unsigned int c, s_constants_t *constants ) {
-
-    complex<float> z = 0.0f;
-    unsigned int n = 0;
-
-    while (abs(z) < 2.0f && ++n < constants->MAX_N) {
-        z = z*z + complex<float>(
-            c * 2.0f / constants->MAX_COL - 1.5f,
-            r * 2.0f / constants->MAX_ROW - 1.0f
-        );
-    }
-
-    // set pixel
-    constants->IMAGE[r*constants->MAX_COL+c] = (n == constants->MAX_N ? '#' : '.');
-
-}
 
 void work( s_constants_t *constants )
 {
 
-    const unsigned int MAX_PIXEL = constants->MAX_COL*constants->MAX_ROW;
-    unsigned int p;
+    const uint MAX_PIXEL = constants->MAX_COL*constants->MAX_ROW;
+    uint step_size = constants->MAX_COL;
+
+    uint p, p_begin, p_end, row, col, n;
+    complex<float> z;
 
     while (true) {
-        pixel_mutex.lock();
-        p = current_pixel++;
-        pixel_mutex.unlock();
+
+        // get next working set
+        p_begin = (current_pixel += step_size);
+        p_begin -= step_size;
+        p_end = min(p_begin+step_size, MAX_PIXEL);
 
         // check if done
-        if (p >= MAX_PIXEL) break;
+        if (p_begin >= MAX_PIXEL) break;
 
-        // calculate next pixel
-        calculate(p / constants->MAX_COL, p % constants->MAX_COL, constants );
+        for (p = p_begin; p < p_end; p++) {
+
+            //if (p % 1000 == 0) cerr << p << endl;
+
+            // prepare variables for next iteration
+            row = p / constants->MAX_COL;
+            col = p % constants->MAX_COL;
+            z = 0.0f;
+            n = 0u;
+
+            // calculate next pixel
+            while (abs(z) < 2.0f && ++n < constants->MAX_N) {
+                z = z*z + complex<float>(
+                    col * 2.0f / constants->MAX_COL - 1.5f,
+                    row * 2.0f / constants->MAX_ROW - 1.0f
+                );
+            }
+
+            // set pixel
+            constants->IMAGE[row*constants->MAX_COL+col] = (n == constants->MAX_N ? '#' : '.');
+
+        }
+
+        // adjust stepsize
+        if (p_end + NUM_CORES*4 > MAX_PIXEL) {
+            step_size = 1;
+        } else {
+            step_size = max(uint(constants->MAX_COL / 2.0 * (1.0 - ((double)p_end/MAX_PIXEL))), 1u);
+        }
+
     }
 
 }
@@ -63,12 +87,12 @@ void work( s_constants_t *constants )
 int main() {
 
     // get amount of cores
-    const auto NUM_CORES = getenv("MAX_CPUS") == nullptr ? thread::hardware_concurrency() : atoi(getenv("MAX_CPUS"));
+    NUM_CORES = getenv("MAX_CPUS") == nullptr ? thread::hardware_concurrency() : atoi(getenv("MAX_CPUS"));
     assert((NUM_CORES > 0 && NUM_CORES <= 56) && "Could not get the number of cores!");
     cerr << "Working with " << NUM_CORES << " cores" << endl;
 
     // read stdin
-    unsigned int max_row, max_col, max_n;
+    uint max_row, max_col, max_n;
     cin >> max_row;
     cin >> max_col;
     cin >> max_n;
@@ -88,18 +112,18 @@ int main() {
 
     // calculate mandelbrot set
     thread *threads = new thread[NUM_CORES];
-    for (unsigned int i = 0; i < NUM_CORES; i++) {
+    for (uint i = 0; i < NUM_CORES; i++) {
         threads[i] = thread(work, &constants );
     }
 
     // wait for them to finish
-    for (unsigned int i = 0; i < NUM_CORES; i++) {
+    for (uint i = 0; i < NUM_CORES; i++) {
         threads[i].join();
     }
 
     // print result
-    for (unsigned int r = 0; r < max_row; ++r) {
-        for (unsigned int c = 0; c < max_col; ++c) cout << img[r*max_col+c];
+    for (uint r = 0; r < max_row; ++r) {
+        for (uint c = 0; c < max_col; ++c) cout << img[r*max_col+c];
         cout << '\n';
     }    
 }
