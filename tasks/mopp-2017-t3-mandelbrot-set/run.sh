f9f0254923a30d9d635bb4d52dd98c87a4acce50
Fritz Louis Wilke <fritz.wilke@tu-dresden.de>
2021-04-23 11:45:36 +0200

    made added run scripts to gitignore

--- tasks/mopp-2017-t3-mandelbrot-set/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-time cat judge.in | MAX_CPUS=1 ./mandelbrot >test.out 2>/dev/null
-if ! cmp -s test.out judge.out ; then echo "Incorrect result!"; fi
-
-time cat judge.in | ./mandelbrot >test.out 2>/dev/null
-if ! cmp -s test.out judge.out ; then echo "Incorrect result!"; fi
