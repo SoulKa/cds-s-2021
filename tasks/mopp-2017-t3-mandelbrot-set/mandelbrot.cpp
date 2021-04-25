cbcc92cc05470ce96fce6f0725f6f3a60bb77d9d
Fritz Louis Wilke <fritz.wilke@tu-dresden.de>
2021-04-25 14:02:41 +0200

    improved mandelbrot by directly creating a nullterminated string

--- tasks/mopp-2017-t3-mandelbrot-set/mandelbrot.cpp
+++ tasks/mopp-2017-t3-mandelbrot-set/mandelbrot.cpp
@@ -12,8 +12,6 @@ typedef struct {
     const uint MAX_ROW;
     const uint MAX_COL;
     const uint MAX_N;
-    const float COL_ITER;
-    const float ROW_ITER;
     char *IMAGE;
 } s_constants_t;
 
@@ -30,36 +28,45 @@ uint NUM_CORES;
 void work( uint thread_number, s_constants_t *constants )
 {
 
-    uint row, col, n;
-    float z_r;
-    float z_i;
-    float tmp;
+    uint p = thread_number*constants->MAX_ROW*(constants->MAX_COL+1)/NUM_CORES;
+    uint p_end = (thread_number+1)*constants->MAX_ROW*(constants->MAX_COL+1)/NUM_CORES;
+    uint n, row, col;
+    float z_r, z_i, z_r_tmp;
 
     // iterate over all pixel that this thread has to calculate
-    for (uint p = thread_number*constants->MAX_ROW*constants->MAX_COL/NUM_CORES; p < (thread_number+1)*constants->MAX_ROW*constants->MAX_COL/NUM_CORES; p++) {
+    for (; p < p_end; p++) {
 
-        // prepare variables for next iteration
-        row = p / constants->MAX_COL;
-        col = p % constants->MAX_COL;
-        z_r = 0.0;
-        z_i = 0.0;
+        
+        row = p / (constants->MAX_COL+1);
+        col = p % (constants->MAX_COL+1);
 
-        // calculate next pixel
-        for (n=1u; n < constants->MAX_N; n++) {
+        if (col < constants->MAX_COL ) {
 
-            // square z
-            tmp = z_r;
-            z_r = z_r*z_r - z_i*z_i;
-            z_i = 2*tmp*z_i;
+            // prepare variables for next iteration
+            z_r = 0.0;
+            z_i = 0.0;
 
-            // add
-            z_r += col * 2.0f / constants->MAX_COL - 1.5f;
-            z_i += row * 2.0f / constants->MAX_ROW - 1.0f;
+            // calculate next pixel
+            for (n=1u; n < constants->MAX_N; n++) {
 
+                // square z
+                z_r_tmp = z_r;
+                z_r = z_r*z_r - z_i*z_i;
+                z_i = 2*z_r_tmp*z_i;
+
+                // add
+                z_r += col * 2.0f / constants->MAX_COL - 1.5f;
+                z_i += row * 2.0f / constants->MAX_ROW - 1.0f;
+
+            }
+
+            // set pixel
+            constants->IMAGE[row*(constants->MAX_COL+1)+col] = sqrt(z_r*z_r + z_i*z_i) < 2.0f ? '#' : '.';
+
+        } else {
+            constants->IMAGE[row*(constants->MAX_COL+1)+col] = '\n';
         }
-
-        // set pixel
-        constants->IMAGE[row*constants->MAX_COL+col] = sqrt(z_r*z_r + z_i*z_i) < 2.0f ? '#' : '.';
+        
 
     }
 
@@ -79,34 +86,26 @@ int main() {
     cin >> max_n;
 
     // create image
-    char *img = new char[max_row*max_col];
+    char *img = new char[max_row*(max_col+1)+1];
+    img[max_row*(max_col+1)] = '\0';
 
     // CONSTANTS
     s_constants_t constants = {
         max_row,
         max_col,
         max_n,
-        2.0f / max_col,
-        2.0f / max_row,
         img
     };
 
     // calculate mandelbrot set
     thread *threads = new thread[NUM_CORES];
-    for (uint i = 0; i < NUM_CORES; i++) {
-        threads[i] = thread(work, i, &constants );
-    }
+    for (uint i = 0; i < NUM_CORES; i++) threads[i] = thread(work, i, &constants );
 
     // wait for them to finish
-    for (uint i = 0; i < NUM_CORES; i++) {
-        threads[i].join();
-    }
+    for (uint i = 0; i < NUM_CORES; i++) threads[i].join();
 
     // print result
-    for (uint r = 0; r < max_row; ++r) {
-        for (uint c = 0; c < max_col; ++c) cout << img[r*max_col+c];
-        cout << '\n';
-    }    
+    printf("%s", img);  
 }
 
 
